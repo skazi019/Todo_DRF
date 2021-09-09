faf5c9d3935c730f08287b9409b24ca852d759c5
Kaushal Sharma <kaushal@Kaushals-MacBook-Pro.local>
2021-09-09 16:41:25 +0530

    registering model in admin

--- todo_drf/api/admin.py
+++ todo_drf/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.db import models
+from .models import Task
 
 # Register your models here.
+class TaskModel(admin.ModelAdmin):
+    model = Task
+
+admin.site.register(TaskModel)
